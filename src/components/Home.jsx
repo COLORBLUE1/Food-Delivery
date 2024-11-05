83296d6181820e03fb601af5967526efaa76e3f8
abel-git <camilosol123@gmail.com>
2024-11-05 14:51:58 -0500

    🦄 refactor:

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,12 @@
 import { Navbar } from "./Blocks/Navbar";
 
-import {
-  Contenedormain
-} from "../assets/style/stylecomponets/styled.js";
+import { Contenedormain } from "../assets/style/stylecomponets/styled.js";
 import { Main } from "./Home/Main.jsx";
-import { useContext } from "react";
-import { UserContext } from "../context/context.js";
-import Admin from "./admin.jsx";
 
 export function Home() {
-  const { user } = useContext(UserContext)
-
   return (
     <Contenedormain>
-      {user.role === "admin" ? <Admin /> : <Main />}
+      <Main />
       <Navbar />
     </Contenedormain>
   );
